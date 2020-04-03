9d06700dc1f086a72217688523c09f89b4dbca81
Dylan <dcren2010@gmail.com>
2020-04-03 16:05:56 -0400

    lc 677.Map Sum Pairs

---
+++ Advanced/677.Map Sum Pairs.js
@@ -0,0 +1,60 @@
+/**
+ * Initialize your data structure here.
+ */
+var MapSum = function() {
+    this.map = {}
+};
+
+/**
+ * @param {string} key
+ * @param {number} val
+ * @return {void}
+ */
+MapSum.prototype.insert = function(key, val) {
+    this.map[key] = val
+};
+
+/**
+ * @param {string} prefix
+ * @return {number}
+ */
+MapSum.prototype.sum = function(prefix) {
+    let count = 0;
+    for(let key in this.map){
+        if(key.indexOf(prefix) === 0) count += this.map[key]
+    }
+    return count
+};
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * var obj = new MapSum()
+ * obj.insert(key,val)
+ * var param_2 = obj.sum(prefix)
+ */
+
+
+class MapSumII{
+    constructor() {
+        this.map = {}
+    }
+
+    insert(key, val){
+        this.map[key] = val
+    }
+
+    mapSum(prefix){
+        let sum = 0;
+        for(let key in this.map){
+            if(key.indexOf(prefix) === 0) sum += this.map[key];
+            // if(prefix === key.substring(0, prefix.length)) sum += this.map[key]
+        }
+        return sum
+    }
+}
+
+const mapSumPair = (key, val, prefix)=> {
+    const ms = new MapSumII();
+    ms.insert(key, val);
+    ms.mapSum(prefix)
+};
