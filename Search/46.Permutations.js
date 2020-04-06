d73ee93ee5573d4ab00a937383fe8d7c9ced8712
Dylan <dcren2010@gmail.com>
2020-04-05 20:51:03 -0400

    46.Permutation

---
+++ Search/46.Permutations.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    let permutations = [];
+
+    let findPermutations = (visited = new Set(), currPerm = []) => {
+        // once loop all the items from nums and the length === nums.length, push them into permutations array
+        if(currPerm.length === nums.length) {
+            permutations.push(currPerm);
+            return;
+        }
+        for(let i = 0; i < nums.length; i ++){
+            // the reason to have new Set() is to use .has method to verify if the key (i) exist or not,
+            // if the object doesn't have the value of this key, then based on the key (i), adding the value to currPerm,
+            // then recursively spread adding key and nums[i] to currPerm
+            if(!visited.has(i)){
+                findPermutations(new Set([...visited, i]), [...currPerm, nums[i]])
+            }
+        }
+    };
+    findPermutations();
+    return permutations
+};
