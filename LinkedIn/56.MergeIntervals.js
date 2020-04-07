9747db6b2623f2bea834d177b623186f8772b1b0
Dylan <dcren2010@gmail.com>
2020-04-07 14:10:59 -0400

    lc 56 MergeIntervals

---
+++ LinkedIn/56.MergeIntervals.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+
+/* ideas:
+    1. first we need to sort the intervals, so that we can compare each interval
+    2. looping through this interval
+    3. we only compare the end number of pre interval and the start number of next interval, if there is overlapping, then merge them also save them into result;
+        if not overlapping, we need to save both intervals into res.
+    4. return res
+ */
+
+var merge = function(intervals) {
+    // for intervals question, sort the intervals first
+    let res = [];
+    // sort intervals is using start time: [0] to get start number, interval end time using: [1] to get end number
+    intervals.sort((a, b) => a[0]-b[0]);
+
+    for(let i = 0; i < intervals.length; i ++ ){
+        // variable pre is the last position interval in the res, it will change when looping i
+        let pre = res[res.length-1];
+        // if only one interval in this arr or there is no overlapping between current interval start number
+        // and pre end number, because interval is sorted, so interval[i] > pre, so we compare pre end number and interval[i] start number
+        // then we push this interval[i] into res []
+        if(i === 0 || intervals[i][0] > pre[1]){
+            res.push(intervals[i])
+        } else {
+            // if there is overlapping, then we choose the max number as the pre end number
+            pre[1] = Math.max(intervals[i][1], pre[1])
+        }
+    }
+    return res
+};
+
