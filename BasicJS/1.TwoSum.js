8625678eecd7fc28c60cd64c11d898022fe1317d
Dylan <dcren2010@gmail.com>
2020-04-07 13:22:18 -0400

    lc 56 Merge intervals

--- BasicJS/1.TwoSum.js
+++ BasicJS/1.TwoSum.js
@@ -1,15 +1,28 @@
-var twoSum = function(nums, target) {
-    let map = {}
+// var twoSum = function(nums, target) {
+//     let map = {};
+//
+//     for(let i =0; i < nums.length; i ++){
+//         let diffNum = target - nums[i];
+//         // if map has this diffNum's value, then return this position and current position of nums
+//         if(map[diffNum] !== undefined){
+//             return [map[diffNum], i]
+//         }
+//         // if map doesn't have this key, then add this position to this map
+//         map[nums[i]] = i
+//     }
+// };
 
-    for(let i =0; i < nums.length; i ++){
+// so in map, using map[] to represent the key and get the value of this object
+// in array, using array[] to represent the value of this position
+
+// practice:
+let twoSum = (nums, target) => {
+    let map = {};
+    for(let i = 0; i < nums.length; i ++){
         let diffNum = target - nums[i];
-        // if map has this diffNum's value, then return this position and current position of nums
         if(map[diffNum] !== undefined){
             return [map[diffNum], i]
         }
-        // if map doesn't have this key, then add this position to this map
         map[nums[i]] = i
     }
 };
-
-// so in map, using [] to get the value of this object
