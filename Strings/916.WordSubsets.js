2f531b7a947acf58253b30806ac4f0f5eec3f81f
Dylan <dcren2010@gmail.com>
2020-04-19 13:23:36 -0400

    lc 916 update

--- Strings/916.WordSubsets.js
+++ Strings/916.WordSubsets.js
@@ -1,49 +1,45 @@
 /**
- * @param {Array<string>} A
- * @param {Array<string>} B
- * @return {Array<string>}
+ * @param {string[]} A
+ * @param {string[]} B
+ * @return {string[]}
  */
-const wordSubsets = (A, B) => {
-    // First, we may assume all words in A & B are lower cases, if not, we can easily convert them to lower cases
-    const bCount = Array(26).fill(0);
+var wordSubsets = function(A, B) {
+    const res = [];
+    const combinedLettersInB = Array(26).fill(0);
 
-    // create a count function for both loops: this function indicates the idx position has letter exists and count its number
-    // And this fun has be written before all other codes if using it:
-    const countFun = (word) => {
+    const countFun = (word)=> {
         const output = Array(26).fill(0);
 
         for(let i = 0; i < word.length; i ++){
-            const letter = word.charAt(i);
+            const letter = word.charAt(i)
 
-            const idx = letter.charCodeAt(0) - 'a'.charCodeAt(0);
-            output[idx] += 1
+            const idx = letter.charCodeAt(0) - "a".charCodeAt(0);
+            output[idx] += 1;
         }
-        return output
+        return output;
     };
 
     for(let i = 0; i < B.length; i ++){
         const wordInB = B[i];
-        const tmpCountOfB = countFun(wordInB);
+        const countOfB = countFun(wordInB);
 
-        for(let j = 0; j < 26; j++){
-            // here is to verify if "aaa" && "aa" exist, "aaa" will replace "aa"
-            bCount[j] = Math.max(bCount[j], tmpCountOfB[j])
+        for(let j = 0; j < 26; j ++){
+            combinedLettersInB[j] = Math.max(combinedLettersInB[j], countOfB[j])
+        }
+    };
+
+    for(let i = 0; i < A.length; i ++){
+        const wordInA = A[i];
+        const countOfA = countFun(wordInA)
+
+        let universal = true;
+        for(let j = 0; j < 26; j ++){
+            if(countOfA[j] < combinedLettersInB[j]) universal = false;
+        }
+
+        if(universal){
+            res.push(wordInA)
         }
     }
-
-    const actualOutput = [];
-    for(let i = 0; i < A.length; i ++){
-       const wordInA = A[i];
-       const tmpCountOfA = countFun(wordInA);
-
-       let universal = true;
-       for(let j = 0; j < 26; j ++){
-           if(tmpCountOfA[j] < bCount[j]) universal = false;
-       }
-
-       if(universal){
-           actualOutput.push(wordInA)
-       }
-    }
-    return actualOutput;
+    return res;
 };
